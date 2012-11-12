9b4e1a589e8233ac9650b6b908b27b44c357c340
Brian H. Ward <glyphrider@gmail.com>
2012-11-12 09:07:47 -0500

    pairing with Jim

--- bowling.erl
+++ bowling.erl
@@ -1,34 +1,53 @@
--module(bowling).
+-module(bowling_kata).
 
--ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
--endif.
 
-score(Rolls) -> score(Rolls,0,{0,1},{0,0}).
+score(Pins) -> score(Pins,0,{1,0,0},{0,0}).
 
-score([ThisBall|T],Result,Frame,Bonus) -> score(T,Result+points(ThisBall,Bonus),next(ThisBall,Frame),bonus(ThisBall,Frame,Bonus));
-score([], Result,_Frame,_Bonus) -> Result.
+score([],Score,_Frame,_Bonus) -> Score;
+score([Pin|TheRest],Score,Frame,Bonus) ->
+	score(TheRest,increment_score(Score,Pin,Frame,Bonus),frame(Frame,Pin),bonus(Bonus,Frame,Pin)).
 
-next(ThisBall,{FrameScore,_}) when ThisBall + FrameScore == 10 -> {0,1};
-next(_ThisBall,{_FrameScore,2}) -> {0,1};
-next(ThisBall,{FrameScore,1}) -> {FrameScore+ThisBall,2}.
+increment_score(CurrentScore,NewPins,Frame,{ThisFrameBonus,_NextFrameBonus}) -> CurrentScore + count_pins(Frame,NewPins) + (NewPins * ThisFrameBonus).
 
-points(ThisBall,{Bonus1,Bonus2}) -> ThisBall+apply_bonus(Bonus1,ThisBall)+apply_bonus(Bonus2,ThisBall).
+count_pins({FrameNumber,_PinsInThisFrame,_ThrowsInThisFrame},NewPins) when FrameNumber =< 10 -> NewPins;
+count_pins(_,_) -> 0.
 
-apply_bonus(0,_) -> 0;
-apply_bonus(_,ThisBall) -> ThisBall.
+frame({FrameNumber,0,0},10) -> {FrameNumber+1,0,0};
+frame({FrameNumber,_PinsInThisFrame,1},_NewPins) -> {FrameNumber+1,0,0};
+frame({FrameNumber,_PinsInThisFrame,ThrowsInThisFrame},NewPins) -> {FrameNumber,NewPins,ThrowsInThisFrame+1}.  
 
-bonus(ThisBall,{FrameScore,FrameBall},{_Bonus1,Bonus2}) when ThisBall + FrameScore == 10 -> {3-FrameBall,Bonus2};
-bonus(_,_,{Bonus1,_Bonus2}) -> {0,decr(Bonus1)}.
+bonus({_ThisFrameBonus,NextFrameBonus},{FrameNumber,0,0},10) when FrameNumber =< 10 -> {1+NextFrameBonus,1};
+bonus(_OldBonus,{FrameNumber,PinsInThisFrame,_ThrowsInThisFrame},NewPins) when PinsInThisFrame + NewPins == 10, FrameNumber =< 10 -> {1,0};
+bonus({_ThisFrameBonus,NextFrameBonus},_,_) -> {NextFrameBonus,0}.
 
-decr(0) -> 0;
-decr(N) -> N-1.
 
--ifdef(EUNIT).
+%%Tests
 
-gutter_game_test() -> 0 = score([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
-one_point_game_test() -> 1 = score([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]).
-spare_game_test() -> 16 = score([1,9,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
-strike_game_test() -> 22 = score([10,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
+gutter_ball_game_test() -> 0 = score([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
+one_pin_game_test() -> 1 = score([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
+two_pin_game_test() -> 2 = score([1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
+all_open_frame_test() -> 70 = score([3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4]).
+game_with_a_spare_test() -> 20 = score([5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
+game_with_a_spare_every_frame_test() -> 150 = score([5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5]).
+game_with_a_strike_test() -> 20 = score([10,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
+game_with_a_strike_and_a_real_second_ball_test() -> 30 = score([10,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
+game_with_seqential_strikes_test() -> 60 = score([10,10,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0]).
+perfect_game_test() -> 300 = score([10,10,10,10,10,10,10,10,10,10,10,10]).
 
--endif.
+frame_test() -> {1,0,1} = frame({1,0,0},0).
+second_throw_frame_test() -> {2,0,0} = frame({1,0,1},0).
+score_in_frame_test() -> {1,1,1} = frame({1,0,0},1).
+score_in_second_throw_frame_test() -> {2,0,0} = frame({1,0,1},1).
+
+spare_bonus_test() -> {1,0} = bonus({0,0},{1,5,1},5).
+strike_bonus_test() -> {1,1} = bonus({0,0},{1,0,0},10).
+strike_after_strike_bonus_test() -> {2,1} = bonus({1,1},{1,0,0},10).
+bonus_rolls_off_test() -> {1,0} = bonus({1,1},{1,0,0},0).
+
+increment_score_test() -> 1 = increment_score(0,1,{1,0,0},{0,0}). 
+increment_score_with_bonus_test() -> 20 = increment_score(10,5,{2,0,0},{1,0}).
+
+count_pins_frame_less_than_ten_test() -> 3 = count_pins({3,2,1},3).
+count_pins_equal_to_ten_test() -> 3 = count_pins({10,2,1},3).
+count_pins_greater_than_ten_test() -> 0 = count_pins({11,2,1},3).
